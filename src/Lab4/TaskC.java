b023182d313751b62d391f768f15da48c847868c
Nikolay <turov96@yandex.ru>
2017-04-25 23:27:58 +0300

    task C

--- src/Lab4/TaskC.java
+++ src/Lab4/TaskC.java
@@ -1,77 +1,86 @@
 package Lab4;
 
+import javafx.util.Pair;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 /**
- * Created by Николай on 10.04.2017
+ * Created by Николай on 25.04.2017.
  */
 public class TaskC {
-
-    private static class Edge {
-        int start;
-        int end;
-        int weight;
-
-        Edge(int s, int e, int w) {
-            this.start  = s;
-            this.end    = e;
-            this.weight = w;
-        }
-    }
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader in = new BufferedReader(new FileReader("pathbgep.in"));
-        BufferedWriter out = new BufferedWriter(new FileWriter("pathbgep.out"));
+        PrintWriter out = new PrintWriter("pathbgep.out");
 
-        StreamTokenizer tokenizer = new StreamTokenizer(in);
-        tokenizer.nextToken();
-        int n = (int) tokenizer.nval;
-        tokenizer.nextToken();
-        int m = (int) tokenizer.nval;
+        String graphSize = in.readLine();
+        StringTokenizer st = new StringTokenizer(graphSize);
 
-        ArrayList<Edge> edges = new ArrayList<>();
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        int s, e, w;
-        for (int i = 0; i < m; i++) {
-            tokenizer.nextToken();
-            s = (int) tokenizer.nval - 1;
-            tokenizer.nextToken();
-            e = (int) tokenizer.nval - 1;
-            tokenizer.nextToken();
-            w = (int) tokenizer.nval;
-
-
-            edges.add(new Edge(s, e, w));
-            edges.add(new Edge(e, s, w));
-
+        ArrayList<ArrayList<Pair<Integer, Long>>> adj = new ArrayList<ArrayList<Pair<Integer, Long>>>(n);
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<Pair<Integer, Long>>());
         }
 
-        int [] d = new int[n];
-        Arrays.fill(d, Integer.MAX_VALUE);
+        int s, e;
+        long weight;
+        String value;
 
-        d[0] = 0;
-        boolean flag = true;
-        while (flag) {
-            flag = false;
-            for (Edge edge : edges) {
-                if (d[edge.start] < Integer.MAX_VALUE) {
-                    if (d[edge.end] > d[edge.start] + edge.weight) {
-                        d[edge.end] = d[edge.start] + edge.weight;
-                        flag = true;
-                    }
+        for (int i = 0; i < m; i++) {
+            value = in.readLine();
+            st = new StringTokenizer(value);
+            s = Integer.parseInt(st.nextToken());
+            e = Integer.parseInt(st.nextToken());
+            weight = Long.parseLong(st.nextToken());
+
+            adj.get(s - 1).add(new Pair<>(e - 1, weight));
+            adj.get(e - 1).add(new Pair<>(s - 1, weight));
+        }
+
+
+
+        long[] dist = new long[n];
+        Arrays.fill(dist, Long.MAX_VALUE);
+
+        dist[0] = 0;
+        Comparator<Pair<Integer, Long>> pair_comparator = new Comparator<Pair<Integer, Long>>() {
+            @Override
+            public int compare(Pair<Integer, Long> o1, Pair<Integer, Long> o2) {
+                return o1.getValue().compareTo(o2.getValue());
+            }
+        };
+
+        PriorityQueue<Pair<Integer, Long>> queue = new PriorityQueue<Pair<Integer, Long>>(pair_comparator);
+
+        queue.add(new Pair<Integer, Long>(0, (long) 0));
+
+        Pair<Integer, Long> vertexWeight, to;
+
+        while (queue.size() != 0) {
+
+            vertexWeight = queue.poll();
+
+            if (vertexWeight.getValue() > dist[vertexWeight.getKey()]) continue;
+
+            for (int j = 0; j < adj.get(vertexWeight.getKey()).size(); j++) {
+                to = adj.get(vertexWeight.getKey()).get(j);
+                if (dist[vertexWeight.getKey()] + to.getValue() < dist[to.getKey()]) {
+                    dist[to.getKey()] = dist[vertexWeight.getKey()] + to.getValue();
+                    queue.add(new Pair(to.getKey(), dist[to.getKey()]));
                 }
             }
+
         }
 
-        for(int i = 0; i < n; i++) {
-            out.write(Integer.toString(d[i]) + " ");
+        for (int i = 0; i < n; i++) {
+            out.print(dist[i] + " ");
         }
 
-        in.close();
         out.close();
+        in.close();
 
     }
 }
